d817ab96c7178b0f9af19790db2b199b1cf52af4
Pawel Hanusik <39835401+pawelhanusik@users.noreply.github.com>
2022-03-15 22:19:23 +0100

    Move Rule::in validation into methods

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Enums\TaskStatusEnum;
 use App\Http\Resources\TaskResource;
 use App\Models\Task;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\Rule;
 
 class TaskController extends StatusModelController
@@ -12,15 +14,34 @@ class TaskController extends StatusModelController
     protected static $storeValidationRules = [
         'computer_id' => ['sometimes', 'required', 'id', 'exists:computers'],
         'title' => ['required', 'string', 'max:125'],
-        'status' => ['sometimes', 'required', Rule::in(TaskStatusEnum::toArray())],
+        'status' => ['sometimes', 'required'],
         'message' => ['sometimes', 'required', 'string'],
     ];
     protected static $updateValidationRules = [
         'computer_id' => ['sometimes', 'required', 'id', 'exists:computers'],
         'title' => ['sometimes', 'required', 'string', 'max:125'],
-        'status' => ['sometimes', 'required', Rule::in(TaskStatusEnum::toArray())],
+        'status' => ['sometimes', 'required'],
         'message' => ['sometimes', 'required', 'string'],
     ];
     protected static $modelClass = Task::class;
     protected static $modelResourceClass = TaskResource::class;
+
+    public function store(Request $request) {
+        $validator = Validator::make($request->all('status'), [
+            'status' => Rule::in(TaskStatusEnum::toArray())
+        ]);
+        if ($validator->fails()) {
+            return $this->api_fail($validator->errors(), 400);
+        }
+        return parent::store($request);
+    }
+    public function update(Request $request, $modelId) {
+        $validator = Validator::make($request->all('status'), [
+            'status' => Rule::in(TaskStatusEnum::toArray())
+        ]);
+        if ($validator->fails()) {
+            return $this->api_fail($validator->errors(), 400);
+        }
+        return parent::update($request, $modelId);
+    }
 }
